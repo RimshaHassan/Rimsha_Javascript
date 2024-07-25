da967d5e319f3c98f7f76f5ccfc93dc399716b9e
RimshaHassan <142317529+RimshaHassan@users.noreply.github.com>
2024-07-25 10:51:26 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,4 @@
+" use strict" // all js  code  treat as the newer version of js 
+// alert(3+3) // we cant use this becuse we are using nodejs not browser
+let a= null; // null is the  object and undefined is datatype
+console.log(a)
--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log("rimsha ")
+//console.log("rimsha ")
+let h= 6899;
+h=67
+console.log(a)
+var a = 77998;
+a =88;
+console.log(a)
+const j=78;
+roadmap= 6666666;
+console.table([h,a,j,roadmap]);
