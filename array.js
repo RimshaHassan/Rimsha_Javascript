bf2faf328b9a519c9ae147c2ebf431a27aec5ab2
RimshaHassan <142317529+RimshaHassan@users.noreply.github.com>
2024-07-26 17:42:02 +0000

    Arrays

---
+++ array.js
@@ -0,0 +1,49 @@
+// // array are resize able 
+// let array = [1,3,"RImsha",787]
+// console.log(array)
+
+
+
+// // we can make array by this method also 
+let myarray = new Array(1,2,4,5,6,7)
+// console.log(myarray)
+// console.log(myarray[3])
+
+
+// we can add and update 
+myarray[2]=" rimsha" // update 
+myarray[6] = " meku"
+// console.log(myarray)
+
+
+
+//**********************method in array  */
+//  myarray.push(6)  // add element at last 
+//  console.log(myarray)
+//  myarray.push(90)  // add element at last 
+//  console.log(myarray)
+//  myarray.pop()  // remove element at last
+//  console.log(myarray) 
+//  myarray.unshift(88)  // add element at startt 
+//  console.log(myarray)
+//  myarray.shift()  // remove element at add 
+//  console.log(myarray)
+//  myarray.pop()  // remove element at last 
+//  console.log(myarray)
+ 
+// console.log(myarray.includes(9)) // ye element array k aandr present hai k ni 
+// console.log(myarray.indexOf(9)) /// element kis index py para hua hai 
+// console.log(myarray.indexOf(1)) /// element kis index py para hua hai 
+console.log(myarray.sort()) // sort alphabetically
+console.log(myarray.toString()) 
+
+
+
+
+// const newarray = myarray.join() // this convert the array into string 
+// console.log(myarray)
+// console.log(newarray, typeof newarray)
+
+
+
+
