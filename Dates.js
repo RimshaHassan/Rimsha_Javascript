fad3f2c0c590da6b29d8188af44a191ced2249b6
RimshaHassan <142317529+RimshaHassan@users.noreply.github.com>
2024-07-26 13:50:46 +0000

    Dates

---
+++ Dates.js
@@ -0,0 +1,16 @@
+//****************DATES**********/
+// let mydate = new Date()
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+// console.log( typeof mydate)
+
+
+
+let createdate = new  Date(2000,12,13)
+// console.log(createdate)
+// console.log(createdate.toDateString())
+
+let mytimestamp = Date.now() // this give date in points or milivalues
+console.log(mytimestamp)
+console.log(createdate.getTime()) // ye exact date to mili values m kr dy ga 
